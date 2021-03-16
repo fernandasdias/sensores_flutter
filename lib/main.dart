a859fcf634dc0f76b945c5890590401463d6901e
Fernanda Dias <fernandasd26@gmail.com>
2021-03-16 20:20:46 -0300

    Working on audio player and sensors

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@
 // ignore_for_file: public_member_api_docs
 
 import 'dart:async';
+import 'dart:math';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
 
@@ -42,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
   static const double _snakeCellSize = 10.0;
 
   List<double> _accelerometerValues;
-  List<double> _userAccelerometerValues;
+  List<int> _userAccelerometerValues;
   List<double> _gyroscopeValues;
   List<StreamSubscription<dynamic>> _streamSubscriptions =
       <StreamSubscription<dynamic>>[];
@@ -54,9 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
     final List<String> gyroscope =
         _gyroscopeValues?.map((double v) => v.toStringAsFixed(1))?.toList();
     final List<String> userAccelerometer = _userAccelerometerValues != null
-        ? _userAccelerometerValues
-            .map((double v) => v.toStringAsFixed(1))
-            .toList()
+        ? _userAccelerometerValues.map((int v) => v.toStringAsFixed(1)).toList()
         : null;
 
     return Scaffold(
@@ -113,8 +114,16 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                ElevatedButton(
-                    onPressed: printData(), child: Text('Send Data')),
+                RaisedButton(onPressed: () => start(), child: Text('Start')),
+              ],
+            ),
+            padding: const EdgeInsets.all(16.0),
+          ),
+          Padding(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RaisedButton(onPressed: () => stop(), child: Text('Stop')),
               ],
             ),
             padding: const EdgeInsets.all(16.0),
@@ -132,9 +141,19 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  static int mult = 100000;
   @override
   void initState() {
     super.initState();
+  }
+
+  void stop() {
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  void start() {
     _streamSubscriptions
         .add(accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
@@ -148,13 +167,37 @@ class _MyHomePageState extends State<MyHomePage> {
     }));
     _streamSubscriptions
         .add(userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-      setState(() {
-        _userAccelerometerValues = <double>[event.x, event.y, event.z];
+      double accelResults =
+          sqrt(event.x * event.x + event.y * event.y + event.z * event.z);
+      setState(() async {
+        print(accelResults.toStringAsFixed(2));
+        if (accelResults >= 4) {
+          var audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
+          AudioPlayer.logEnabled = true;
+          AudioCache player = new AudioCache();
+          await audioPlayer.play('assets/correct_sound.mp3');
+        }
+        _userAccelerometerValues = <int>[
+          (event.x * mult).truncate(),
+          (event.y * mult).truncate(),
+          (event.z * mult).truncate()
+        ];
       });
+      milliseconds.add(DateTime.now().millisecond);
+
+      new_time = DateTime.now().millisecond - old; //difference
+
+      old = new_time;
     }));
   }
 
+  int old = DateTime.now().millisecond;
+  int new_time = 0;
+  int difference = 0;
+  static List<int> milliseconds = new List<int>();
   printData() {
-    print(_userAccelerometerValues);
+    // print(
+    //     '\n old time  ' + old.toString() + '  new time ' + new_time.toString());
+    // print(_userAccelerometerValues);
   }
 }
